eb7c02cb8506ddc82566c81a7099492753c8d9c0
Raymond Butcher <ray.butcher@claranet.uk>
2018-02-03 19:26:23 +0000

    Fix splat syntax
    It was breaking with Terraform 0.11.3

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "function_arn" {
   description = "The ARN of the Lambda function"
-  value       = "${lookup(element(concat(aws_lambda_function.lambda.*, aws_lambda_function.lambda_with_dl.*, aws_lambda_function.lambda_with_vpc.*, aws_lambda_function.lambda_with_dl_and_vpc.*), 0), "arn")}"
+  value       = "${element(concat(aws_lambda_function.lambda.*.arn, aws_lambda_function.lambda_with_dl.*.arn, aws_lambda_function.lambda_with_vpc.*.arn, aws_lambda_function.lambda_with_dl_and_vpc.*.arn), 0)}"
 }
 
 output "function_name" {
   description = "The name of the Lambda function"
-  value       = "${lookup(element(concat(aws_lambda_function.lambda.*, aws_lambda_function.lambda_with_dl.*, aws_lambda_function.lambda_with_vpc.*, aws_lambda_function.lambda_with_dl_and_vpc.*), 0), "function_name")}"
+  value       = "${element(concat(aws_lambda_function.lambda.*.function_name, aws_lambda_function.lambda_with_dl.*.function_name, aws_lambda_function.lambda_with_vpc.*.function_name, aws_lambda_function.lambda_with_dl_and_vpc.*.function_name), 0)}"
 }
 
 output "role_arn" {
