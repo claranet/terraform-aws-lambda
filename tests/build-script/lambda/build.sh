4a31cf68db8dbe599b96d2eee832ca3dcc15d01c
Raymond Butcher <ray.butcher@claranet.uk>
2019-01-04 15:57:53 +0000

    Fix Docker permission issues
    When I tried this, it was creating the Lambda build files as the root user and then it failed trying to clean them up. This now does everything including setting ownership inside Docker.
    
    I also removed the default values because I'd rather it break then default to something that may not be intended.

--- tests/build-script/lambda/build.sh
+++ tests/build-script/lambda/build.sh
@@ -22,39 +22,25 @@
 #
 # - Arguments to be provided as JSON, then Base64 encoded
 
-set -e
+set -euo pipefail
 
 # Extract variables from the JSON-formatted, Base64 encoded input argument.
 # This is to conform to arguments as passed in by hash.py
 eval "$(echo $1 | base64 --decode | jq -r '@sh "FILENAME=\(.filename) RUNTIME=\(.runtime) SOURCE_PATH=\(.source_path)"')"
 
-# Apply default values for missing arguments
-FILENAME="${FILENAME:-out.zip}"
-RUNTIME="${RUNTIME:-python3.7}"
-SOURCE_PATH="${SOURCE_PATH:-.}"
-
 # Convert to absolute paths
-FILEPATH="$(cd "$(dirname "$FILENAME")"; pwd)/$(basename "$FILENAME")"
-SOURCE_PATH="$(cd "$SOURCE_PATH"; pwd)"
-
-# Setup a temporary path for the build
-TEMP_BUILD_PATH=$(mktemp -d "/tmp/build-lambda.XXXXXXXXX")
-trap "{ rm -rf '$TEMP_BUILD_PATH'; }" EXIT
-
-# Copy source code
-cp "$SOURCE_PATH"/*.py "$SOURCE_PATH/requirements.txt" "$TEMP_BUILD_PATH"/
+SOURCE_DIR=$(cd "$SOURCE_PATH" && pwd)
+ZIP_DIR=$(cd "$(dirname "$FILENAME")" && pwd)
+ZIP_NAME=$(basename "$FILENAME")
 
 # Install dependencies, using a Docker image to correctly build native extensions
-docker run --rm -t -v "$TEMP_BUILD_PATH:/code" -w /code lambci/lambda:build-$RUNTIME \
-    sh -c "pip install -r requirements.txt -t ."
+docker run --rm -t -v "$SOURCE_DIR:/src" -v "$ZIP_DIR:/out" lambci/lambda:build-$RUNTIME sh -c "
+    cp -r /src /build &&
+    cd /build &&
+    pip install -r requirements.txt -t . &&
+    chmod -R 755 . &&
+    zip -r /out/$ZIP_NAME * &&
+    chown \$(stat -c '%u:%g' /out) /out/$ZIP_NAME
+"
 
-# Required by AWS Lambda
-chmod -R 755 "$TEMP_BUILD_PATH"
-
-# Cleanup old build output
-[ -f "$FILEPATH" ] && rm "$FILEPATH"
-
-# Build zip package with files at root
-(cd "$TEMP_BUILD_PATH"; zip -r $FILEPATH *)
-
-echo "Created $FILEPATH from $SOURCE_PATH"
+echo "Created $FILENAME from $SOURCE_PATH"
