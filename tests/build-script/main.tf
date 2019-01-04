f0ee6945f8e1712a5a4463fc730dbddbc469f5e3
Raymond Butcher <ray.butcher@claranet.uk>
2019-01-04 11:35:03 +0000

    Rename build script test to match variable name

--- tests/build-script/main.tf
+++ tests/build-script/main.tf
@@ -11,11 +11,11 @@ provider "aws" {
 module "lambda" {
   source = "../../"
 
-  function_name = "terraform-aws-lambda-test-custom-build-script"
+  function_name = "terraform-aws-lambda-test-build-script"
   description   = "Test custom build script functionality in terraform-aws-lambda"
   handler       = "main.lambda_handler"
   runtime       = "python3.7"
 
-  source_path = "${path.module}/lambda"
+  source_path  = "${path.module}/lambda"
   build_script = "${path.module}/lambda/build.sh"
 }
