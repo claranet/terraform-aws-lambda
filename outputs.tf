d51ac08f46f2fa73b4cf34674a1d6d05c755bad4
Mark THEBAULT <mark.thebault+infoproj@gmail.com>
2019-03-28 17:46:51 +0100

    add invoke_arn in outputs
    This feature is particularly useful with API Gateway

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "function_arn" {
   value       = "${element(concat(aws_lambda_function.lambda.*.arn, aws_lambda_function.lambda_with_dl.*.arn, aws_lambda_function.lambda_with_vpc.*.arn, aws_lambda_function.lambda_with_dl_and_vpc.*.arn), 0)}"
 }
 
+output "function_invoke_arn" {
+  description = "The Invoke ARN of the Lambda function"
+  value       = "${element(concat(aws_lambda_function.lambda.*.invoke_arn, aws_lambda_function.lambda_with_dl.*.invoke_arn, aws_lambda_function.lambda_with_vpc.*.invoke_arn, aws_lambda_function.lambda_with_dl_and_vpc.*.invoke_arn), 0)}"
+}
+
 output "function_name" {
   description = "The name of the Lambda function"
   value       = "${element(concat(aws_lambda_function.lambda.*.function_name, aws_lambda_function.lambda_with_dl.*.function_name, aws_lambda_function.lambda_with_vpc.*.function_name, aws_lambda_function.lambda_with_dl_and_vpc.*.function_name), 0)}"
