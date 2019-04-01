9d6d54f264cb7ce010dc17deb5a6548432174020
Loren Gordon <lorengordon@users.noreply.github.com>
2019-04-01 13:27:46 -0400

    Applies tags to iam role resource

--- iam.tf
+++ iam.tf
@@ -15,6 +15,7 @@ data "aws_iam_policy_document" "assume_role" {
 resource "aws_iam_role" "lambda" {
   name               = "${var.function_name}"
   assume_role_policy = "${data.aws_iam_policy_document.assume_role.json}"
+  tags               = "${var.tags}"
 }
 
 # Attach a policy for logs.
