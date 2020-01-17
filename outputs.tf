4380caf5ec765988420a1deb617ede67aa5e879d
Nick Doyle <nick.doyle@intelematics.com>
2020-01-17 13:39:11 +1100

    fix: outputs, using element() on splats of lambdas, concat list of empty string on end, to avoid case where both lambda splats are empty hence zero-index of nothing breaks (now will return empty string)"

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "function_arn" {
   description = "The ARN of the Lambda function"
-  value       = element(concat(aws_lambda_function.lambda.*.arn, aws_lambda_function.lambda_from_s3.*.arn), 0)
+  value       = element(concat(aws_lambda_function.lambda.*.arn, aws_lambda_function.lambda_from_s3.*.arn, list("")), 0)
 }
 
 output "function_invoke_arn" {
   description = "The Invoke ARN of the Lambda function"
-  value       = element(concat(aws_lambda_function.lambda.*.invoke_arn, aws_lambda_function.lambda_from_s3.*.invoke_arn), 0)
+  value       = element(concat(aws_lambda_function.lambda.*.invoke_arn, aws_lambda_function.lambda_from_s3.*.invoke_arn, list("")), 0)
 }
 
 output "function_name" {
   description = "The name of the Lambda function"
-  value       = element(concat(aws_lambda_function.lambda.*.function_name, aws_lambda_function.lambda_from_s3.*.function_name), 0)
+  value       = element(concat(aws_lambda_function.lambda.*.function_name, aws_lambda_function.lambda_from_s3.*.function_name, list("")), 0)
 }
 
 output "function_qualified_arn" {
   description = "The qualified ARN of the Lambda function"
-  value       = element(concat(aws_lambda_function.lambda.*.qualified_arn, aws_lambda_function.lambda_from_s3.*.qualified_arn), 0)
+  value       = element(concat(aws_lambda_function.lambda.*.qualified_arn, aws_lambda_function.lambda_from_s3.*.qualified_arn, list("")), 0)
 }
 
 output "role_arn" {
