f9ff6ee0788e187327c182f691041fbc9527e080
Fabrizio Romanelli <fabrizioromanelli@gmail.com>
2020-02-18 19:46:35 +0100

    New feature - Additional trust relashionships (#59)
    * Adding cloudwatch event role for trust relationship lambda
    
    * Adding assume_roles
    
    * Adding few comments and finalizing
    
    * Removing useless comments
    
    * Updating README.md + adding tests
    
    * Update variables.tf
    
    * Update iam.tf
    
    * Update README.md
    
    * Update main.tf

--- variables.tf
+++ variables.tf
@@ -51,6 +51,12 @@ variable "policy" {
   default = null
 }
 
+variable "trusted_entities" {
+  description = "Lambda function additional trusted entities for assuming roles (trust relationship)"
+  type = list(string)
+  default = []
+}
+
 locals {
   publish = var.lambda_at_edge ? true : var.publish
   timeout = var.lambda_at_edge ? min(var.timeout, 5) : var.timeout
